52264066d83dcd3303f1d34f3f67ef46c3a2f5a9
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-04 20:17:27 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,7 +19,7 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.each do |combo|
     combo.each do |index|
-      if position_taken?(board, index)
+      if position_taken?(board, index) == WIN_COMBINATIONS.any?
       else return false
       end
     end
